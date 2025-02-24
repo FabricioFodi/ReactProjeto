55065161148f9e6e1d58d7e8979c7bb10e6f7ae4
Fabricio Fodi <fabricio_fodi@estudante.sc.senai.br>
2025-02-24 17:15:58 -0300

    estilização

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,19 @@
 import '../App.css'
 import Header from './header.js'
+import '../backend/styles/page1.css'
 
 
 function Home() {
-  
+
   return (
     <>
+      <Header />
       <div className='tudo'>
-        <Header />
-        <div className="pagina-principal">
-          <main className='homepage'>
-            <div className='titulo-da-pagina'>
-              <h1>Anotando os dados dos novos celulares</h1>
-            </div> {/* fim div conteudo */}
-          </main>
-        </div> {/* fim div pagina-principal */}
+        <main className='homepage'>
+          <div className='titulo-da-pagina'>
+            <h1>ANOTANDO<br/> OS DADOS DOS NOVOS<br/> <span>CELULARES</span></h1>
+          </div> {/* fim div conteudo */}
+        </main>
       </div > {/* fim div tudo */}
     </>
   )
