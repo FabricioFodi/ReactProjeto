bcdb7e82d2f43a517cef3cfc52bf4ddbdc5dc2c4
Fabricio Fodi <fabricio_fodi@estudante.sc.senai.br>
2025-02-26 17:19:59 -0300

    estilização 3.0

--- src/pages/consulta.tsx
+++ src/pages/consulta.tsx
@@ -1,19 +1,19 @@
 import Header from './header.js';
+import '../backend/styles/pagina-consulta.css';
 
 function Consulta() {
     return (
-        <div className="tudo-consulta">
+        <>
             <Header />
-            <header className="header-content">
-                <h1>Consulta</h1>
-            </header>
-            <main className='main-content'>
-                <p>Para visualizar os celulares cadastrados, consulte a pasta de TI no seguinte caminho: TI:\Telefones Celulares\Aparelhos com Funcionários 2022 2</p>
-            </main>
-            <footer className="footer-content">
-                <p>Desenvolvido por Fabricio Fodi</p>
-            </footer>
-        </div>
+            <div className="tudo-consulta">
+                <header className="header-container">
+                    <h1>CONSULTAR</h1>
+                </header>
+                <main className='main-container'>
+                    <p>Para visualizar os celulares cadastrados, consulte a pasta <strong> T.I </strong>no seguinte caminho: <strong> TI:\Telefones Celulares\Aparelhos com Funcionários 2022 2</strong></p>
+                </main>
+            </div>
+        </>
     )
 }
 export default Consulta
--- src/pages/recommended.tsx
+++ src/pages/recommended.tsx
@@ -1,31 +1,31 @@
-import '../App.css'
 import Header from './header.js'
+import '../backend/styles/page-recomendado.css'
 
 function Recommended() {
 
     return (
         <>
+            <Header />
             <div className="tudo-recomendacoes">
-                <Header />
-                <div className="pagina-principal">
-                    <main className="homepage">
-                        <div className="titulo-da-pagina">
-                            <h1>Recomendações</h1>
+                <div className="pagina-recomendacoes">
+                    <main className="homepage-recomendacoes">
+                        <div className="titulo-da-pagina-recomendacoes">
+                            <h1>RECOMENDAÇÕES</h1>
                         </div> {/* fim div titulo-da-pagina */}
-                        <div className="conteudo">
+                        <div className="conteudo-recomendacoes">
                             <h2>Backup do WhatsApp</h2>
-                            <p>Para fazer o backup do WhatsApp, vá em Configurações {'>'} Conversas {'>'} Backup de conversas.
+                            <p>Para fazer o backup do WhatsApp, vá em <code> Configurações {'>'} Conversas {'>'} Backup de conversas.</code>
                                 Lá deve-se escolher a opção <strong>Diariamente</strong> assim quando houver a necessidade ou em casos de acidentes com o celular
                                 não sera perdido quase nenhuma conversa, imagens e afins.</p>
                             <h2>Sincronização do celular</h2>
-                            <p>Para sincronizar o celular com o google, vá para Configurações {'>'} Contas e Backup {'>'} Gerencias contas.
+                            <p>Para sincronizar o celular com o google, vá para <code> Configurações {'>'} Contas e Backup {'>'} Gerencias contas.</code>
                                 Escolha a conta e clique em  <strong>sincronizar a conta</strong>, depois clique nos três pontinhos e aperte <strong>Sincronizar agora</strong>.</p>
-                                <h2>Sincronizar Contatos</h2>
-                                <p>Antes de tirar o chip do celular deverá ser feito uma <strong>Sincronização dos contatos</strong>, para sincronizar os contatos, vá para Contatos {'>'} 
-                                    Gerenciar Contatos {'>'} Sincronizar Contatos. Caso seja um telefone mais antigo, deverá ser feito um <i>backup</i> dos contatos no chip.
-                                </p>
-                                <h2>Atualizações</h2>
-                                <p>Depois de configurar um celular, sempre verifique se há atualizações de <strong>Sistema</strong> ou dos <strong>aplicativos do sistema</strong></p>                   
+                            <h2>Sincronizar Contatos</h2>
+                            <p>Antes de tirar o chip do celular deverá ser feito uma <strong>Sincronização dos contatos</strong>, para sincronizar os contatos, vá para <code> Contatos {'>'} Gerenciar 
+                                Contatos {'>'} Sincronizar Contatos.</code> Caso seja um telefone mais antigo, deverá ser feito um <i>backup</i> dos contatos no chip.
+                            </p>
+                            <h2>Atualizações</h2>
+                            <p>Depois de configurar um celular, sempre verifique se há atualizações de <strong>Sistema</strong> ou dos <strong>aplicativos do sistema</strong></p>
                         </div> {/* fim div conteudo */}
                     </main> { /* fim main */}
                 </div> {/* fim div pagina-principal */}
